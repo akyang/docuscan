1b7067633c5d34f1c7b7158304c08a35ddd19eeb
Ubuntu <ubuntu@ip-172-31-59-223.ec2.internal>
2019-10-26 15:07:18 +0000

    update

--- application.py
+++ application.py
@@ -10,11 +10,11 @@ config.read('config.ini')
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'htm', 'html'])
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = config['flask']['upload_folder']
-app.config['EMBED_FOLDER'] = config['flask']['embed_folder']
-app.config['QRCODE_FOLDER'] = config['flask']['qrcode_folder']
-app.config['SECRET_KEY'] = config['flask']['secret']
+application = Flask(__name__)
+application.config['UPLOAD_FOLDER'] = config['flask']['upload_folder']
+application.config['EMBED_FOLDER'] = config['flask']['embed_folder']
+application.config['QRCODE_FOLDER'] = config['flask']['qrcode_folder']
+application.config['SECRET_KEY'] = config['flask']['secret']
 
 
 def allowed_file(filename):
@@ -26,19 +26,19 @@ def embed_qrcode(filename):
     data = uuid.uuid4().hex
     qr = qrcode.make(data)
     qr_fname = data + '.png'
-    qr_path = app.config['QRCODE_FOLDER'] + qr_fname
+    qr_path = application.config['QRCODE_FOLDER'] + qr_fname
     qr.save(qr_path)
 
-    file = open(app.config['UPLOAD_FOLDER'] + filename, 'r')
+    file = open(application.config['UPLOAD_FOLDER'] + filename, 'r')
     content = file.read().rsplit('</body>', 1)
     img_tag = '<img src=\"{qrcode}\">'.format(qrcode= '../' + qr_path)
     embedded = content[0] + img_tag + '\n</body>' + content[1]
-    f = open(app.config['EMBED_FOLDER'] + filename, 'w+')
+    f = open(application.config['EMBED_FOLDER'] + filename, 'w+')
     f.write(embedded)
     return f
 
 
-@app.route('/', methods=['GET', 'POST'])
+@application.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -53,10 +53,10 @@ def upload_file():
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(filename):
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename))
             embedded_file = embed_qrcode(filename)
             return redirect(request.url)
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    application.run(host='0.0.0.0', port=80)
